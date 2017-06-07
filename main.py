b9e2f720731982cfcb663033c02100b49007cf24
arash parnia <arashparnia@gmail.com>
2017-06-07 11:58:56 +0200

    chapter 2

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-from pandas import scatter_matrix
-from pandas.io.parsers import read_csv
-from rpy2.robjects import r, pandas2ri
-from sklearn import preprocessing, model_selection
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
-
-from boruta import BorutaPy
-from pprint import pprint
-from matplotlib import interactive
-
-
-import matplotlib.pyplot as plt
-import rpy2.robjects as robjects
-# import pandas.rpy.common as com
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn.cluster as cluster
-import time
-
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
+# from pandas import scatter_matrix
+# from pandas.io.parsers import read_csv
+# from rpy2.robjects import r, pandas2ri
+# from sklearn import preprocessing, model_selection
+# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.model_selection import cross_val_score
+#
+# from boruta import BorutaPy
+# from pprint import pprint
+# from matplotlib import interactive
+#
+#
+# import matplotlib.pyplot as plt
+# import rpy2.robjects as robjects
+# # import pandas.rpy.common as com
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import sklearn.cluster as cluster
+# import time
+#
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.svm import SVC
+# from sklearn.tree import DecisionTreeClassifier
 
 
 
@@ -44,4 +44,5 @@ print("main")
 # plt.show()
 
 import PythonCode.Chapter2.CreateDataset
-PythonCode.CreateDataset()
+db= PythonCode.Chapter2.CreateDataset.CreateDataset('../Data/csv-participant-one',0)
+print(db)
